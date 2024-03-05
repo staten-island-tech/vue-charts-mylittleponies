278ecdef0b0bccebd5c711bdf9ade3976923f974
MandyyChen <mandychen022@gmail.com>
2024-03-05 09:59:19 -0500

    SPIKE THE DRAGOON

---
+++ vue-project/src/components/BarChart.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
