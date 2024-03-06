b057fdad3c9929195462903dbe75c5c7c99d014e
MelanieHuangg <melanie.huang0306@gmail.com>
2024-03-06 10:02:26 -0500

    api link

---
+++ vue-project/src/components/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div></div>
+</template>
+
+<script setup>
+import {ref, onMounted} from "vue";
+const scores = ref("");
+async function getScores() {
+    let res = await fetch("https://data.cityofnewyork.us/resource/unse-x4pq.json");
+    let data = await res.json();
+    scores.value = data;
+}
+
+onMounted(() => {
+    getScores();
+});
+</script>
+
+
+<style scoped></style>
