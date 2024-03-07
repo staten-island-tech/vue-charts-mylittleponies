e137406771e393a564ed7668b2cd3b276bb0d6ee
MandyyChen <mandychen022@gmail.com>
2024-03-07 10:10:24 -0500

    PRINCESS LUNA

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -39,6 +39,24 @@ header {
   margin: 0 auto 2rem;
 }
 
+.wrapper a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+.wrapper a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+.wrapper a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+.wrapper a:first-of-type {
+  border: 0;
+}
+
 @media (min-width: 1024px) {
   header {
     display: flex;
