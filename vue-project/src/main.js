308d1588e844752c900fb0c72fc73194a1558000
MandyyChen <mandychen022@gmail.com>
2024-03-07 09:32:45 -0500

    RAINBOWDASH

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,30 +1,6 @@
-// import './assets/main.css'
+import './assets/main.css'
 
-// import { createApp } from 'vue'
-// import App from './App.vue'
+import { createApp } from 'vue'
+import App from './App.vue'
 
-// createApp(App).mount('#app')
-
-// import { createRouter, createWebHistory } from 'vue-router'
-
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes: [
-
-//     {
-//       path: '/piegraph',
-//       name: 'piegraph',
-//       // route level code-splitting
-//       // this generates a separate chunk (About.[hash].js) for this route
-//       // which is lazy-loaded when the route is visited.
-//       component: () => import('../icons/PieGraph.vue')
-//     },
-//     { 
-//       path: '/barchart',
-//       name: 'barchart',
-//       component: () => import('../components/BarChart.vue')
-//     },
-//   ]
-// })
-
-// export default router
+createApp(App).mount('#app')
