e7e0f187a7434181149c2190486d3e20644412a4
MandyyChen <mandychen022@gmail.com>
2024-03-08 09:42:46 -0500

    APPPLEEJACK

---
+++ vue-project/src/ChartView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import BarChart from './components/BarChart.vue'
+</script>
+
+
+<template>
+ <h2> </h2>
+ <BarChart />
+
+
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: { BarChart }
+}
+</script>
+
+<style>
+
+</style>
