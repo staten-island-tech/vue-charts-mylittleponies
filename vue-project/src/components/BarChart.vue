0e7014d752f3c1056005f02ab8c4dcff60633e8d
MelanieHuangg <melanie.huang0306@gmail.com>
2024-03-11 10:14:31 -0400

    combining chart and api view

--- vue-project/src/components/BarChart.vue
+++ vue-project/src/components/BarChart.vue
@@ -1,9 +1,7 @@
-   <template>
-  <Bar
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-  />
+<template>
+  <div class="container">
+    <Bar v-if="loaded" :data="chartData" />
+  </div>
 </template>
 
 <script>
@@ -15,17 +13,21 @@ ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 export default {
   name: 'BarChart',
   components: { Bar },
-  data() {
-    return {
-      chartData: {
-        labels: [ 'Grade', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
-      },
-      chartOptions: {
-        responsive: true
-      }
+  data: () => ({
+    loaded: false,
+    chartData: []
+  }),
+  async mounted () {
+    this.loaded = false
+
+    try {
+      const { userlist } = await fetch("https://data.cityofnewyork.us/resource/unse-x4pq.json")
+      this.chartdata = userlist
+
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
     }
   }
 }
-
 </script>
