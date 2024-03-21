c37c930f579fdba330f27b7a716998dea666df6e
MandyyChen <mandychen022@gmail.com>
2024-03-21 10:15:26 -0400

    pee

--- vue-project/src/PieView.vue
+++ vue-project/src/PieView.vue
@@ -1,12 +1,19 @@
+<script setup>
+import PieChart from './components/PieGraph.vue'
+</script>
 
 <template>
 <h2> Pie Pie Pie</h2>
-
+<PieChart></PieChart>
 </template>
 
 
 
-
+<script>
+export default {
+    name: 'App',
+    components: {PieChart}
+}
 </script>
 
 <style>
--- vue-project/src/components/PieGraph.vue
+++ vue-project/src/components/PieGraph.vue
@@ -1,14 +1,14 @@
 <template>
   <Pie= 
-  id="my-chart-id"
-  :options="chartOptions"
-  :data="chartData"
+  :options="pieOptions"
+  :data="pieData"
   />
 </template>
 
-<script>
-import {Bar} from 
+<script lang ="ts">
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Pie } from 'vue-chartjs'
+import * as chartConfig from './chartConfig.js'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
