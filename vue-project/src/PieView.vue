fc25a8d47915b515f3da7cc1c62d4bfa5fdd00de
MelanieHuangg <melanie.huang0306@gmail.com>
2024-03-21 10:05:23 -0400

    Revert "Merge branch 'main' of https://github.com/staten-island-tech/vue-charts-mylittleponies"
    This reverts commit 2591456d69c9669187f50373e0686ffbe6801b52, reversing
    changes made to cac9b17ce529cd251358b14af7da1317b468d248.

--- vue-project/src/PieView.vue
+++ vue-project/src/PieView.vue
@@ -1,17 +1,12 @@
-<script setup>
-import PieChart from './components/PieGraph.vue'
-</script>
 
 <template>
-<h2> </h2>
-<PieChart></PieChart>
+<h2> Pie Pie Pie</h2>
+
 </template>
 
-<script>
-export default {
-  name: 'App',
-  components: { PieChart }
-}
+
+
+
 </script>
 
 <style>
