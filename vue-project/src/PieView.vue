cac9b17ce529cd251358b14af7da1317b468d248
MelanieHuangg <melanie.huang0306@gmail.com>
2024-03-21 09:51:45 -0400

    barchart

--- vue-project/src/PieView.vue
+++ vue-project/src/PieView.vue
@@ -1,27 +1,11 @@
 
 <template>
 <h2> Pie Pie Pie</h2>
-<Pie :data="data" :options="options" />
+
 </template>
 
 
-<script lang="ts">
-import { Chart } from 'chart.js';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-import { Pie } from 'vue-chartjs'
-import * as chartConfig from './components/PieGraph.vue'
 
-ChartJS.register(ArcElement, Tooltip, Legend)
-
-export default {
-    name: 'App',
-    componenets: {
-        Pie
-    },
-    data() {
-        return chartConfig
-    },
-}
 
 </script>
 
