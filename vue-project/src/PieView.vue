4cd8fdf130e7ce508f48e66997e11213e04bf9ad
MandyyChen <mandychen022@gmail.com>
2024-03-22 09:41:48 -0400

    DONT

--- vue-project/src/PieView.vue
+++ vue-project/src/PieView.vue
@@ -4,40 +4,17 @@ import PieChart from './components/PieGraph.vue'
 
 <template>
 <h2> Pie Pie Pie</h2>
-<<<<<<< HEAD
-<Pie :data="data" :options="options" />
-=======
-
-<h2> </h2>
 <PieChart></PieChart>
->>>>>>> parent of 909f3e3 (Merge branch 'main' of https://github.com/staten-island-tech/vue-charts-mylittleponies)
 </template>
+
 
 <script>
 
-<<<<<<< HEAD
-<script lang="ts">
-import { Chart } from 'chart.js';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-import { Pie } from 'vue-chartjs'
-import * as chartConfig from './components/PieGraph.vue'
-
-ChartJS.register(ArcElement, Tooltip, Legend)
-
 export default {
     name: 'App',
-    componenets: {
-        Pie
-    },
-    data() {
-        return chartConfig
-    },
+    components: {
+        PieChart
+    }
 }
 
-=======
-export default {
-  name: 'App',
-  components: { PieChart }
-}
->>>>>>> parent of 909f3e3 (Merge branch 'main' of https://github.com/staten-island-tech/vue-charts-mylittleponies)
 </script>
--- vue-project/src/components/PieGraph.vue
+++ vue-project/src/components/PieGraph.vue
@@ -6,7 +6,6 @@
 <script lang="ts">
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Pie } from 'vue-chartjs'
-import * as chartConfig from './chartConfig.js'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
