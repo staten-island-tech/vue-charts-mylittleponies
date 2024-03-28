17091ef5abc25682ff04e7599b4d88f985944926
MandyyChen <mandychen022@gmail.com>
2024-03-28 10:11:59 -0400

    GRINCHIES

--- vue-project/src/DataView.vue
+++ vue-project/src/DataView.vue
@@ -1,3 +1,24 @@
 <template>
     <h2> API DATA </h2>
+  <div v-if="!data">Loading...</div>
+  <p v-else> {{ data }}</p>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        data: null,
+      };
+    },
+    methods: {
+      async fetchData() {      
+        const response = await fetch("https://data.cityofnewyork.us/resource/unse-x4pq.json");      
+        this.data = await response.json();
+      },    
+    },
+    mounted() {
+      this.fetchData();
+    },
+  };
+</script>
