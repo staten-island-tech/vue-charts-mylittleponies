9b5f371e74f55e6fccf56acb291d0486836ef86e
Mandy <139280587+MandyyChen@users.noreply.github.com>
2024-04-02 00:41:13 -0400

    IFINISHEDBAR

--- vue-project/src/ChartView.vue
+++ vue-project/src/ChartView.vue
@@ -1,36 +1,66 @@
-<script setup>
-import BarChart from './components/BarChart.vue'
-</script>
-
-
 <template>
-
- <BarChart></BarChart>
-
-
+  <BarChart 
+    v-if="loaded" 
+    :nativeCount="nativeCount" 
+    :asianCount="asianCount" 
+    :blackCount="blackCount" 
+    :latinxCount="latinxCount" 
+    :whiteCount="whiteCount" 
+    :multiCount="multiCount" 
+    :unknownCount="unknownCount"
+  />
 </template>
 
 <script>
+import BarChart from './components/BarChart.vue'
 
 export default {
-  name: 'App',
-  components: { BarChart },
-
-  
+  name: 'Bar',
+  components: {
+    BarChart
+  },
   data() {
     return {
-
-      // loaded: false,
+      loaded: false,
+      nativeCount: [],
+      asianCount: [],
+      blackCount: [],
+      latinxCount: [],
+      whiteCount: [],
+      multiCount: [],
+      unknownCount: []
     }
   },
+  async mounted() {
+    await this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      this.loaded = false;
+      try {
+        const response = await fetch('https://data.cityofnewyork.us/resource/unse-x4pq.json');
+        const data = await response.json();
+        console.log('Raw data:', data);
 
-  // mounted: function(){
-  //   this.fetchData();
-  // }
+        const firstObject = data[0];
+        console.log('First object:', firstObject);
+
+        this.nativeCount = parseInt(firstObject.native_american.replace(',', ''));
+        this.asianCount = parseInt(firstObject.asian.replace(',', ''));
+        this.blackCount = parseInt(firstObject.black.replace(',', ''));
+        this.latinxCount = parseInt(firstObject.latinx.replace(',', ''));
+        this.whiteCount = parseInt(firstObject.white.replace(',', ''));
+        this.multiCount = parseInt(firstObject.multi_racial.replace(',', ''));
+        this.unknownCount = parseInt(firstObject.unknown.replace(',', ''));
+
+        this.loaded = true;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
 }
-
 </script>
-
 <style>
 
 .dropDown{
--- vue-project/src/components/BarChart.vue
+++ vue-project/src/components/BarChart.vue
@@ -1,49 +1,65 @@
 <template>
   <div class="container">
-    <!-- <Bar v-if="loaded" :data="chartData" /> -->
-    <Bar :data="chartData" :options="chartOptions"/>
+    <Bar :data="chartData" :options="chartOptions" />
   </div>
 </template>
 
 <script>
 import { Bar } from 'vue-chartjs';
-// import {ref, onMounted} from "vue";
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-
-async function apiData(){
-  const URL = "https://data.cityofnewyork.us/resource/unse-x4pq.json";
-
-  const response = await fetch(URL)
-  const dataPoints = await response.json()
-  console.log(dataPoints)
-}
-apiData()
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
 export default {
   name: 'BarChart',
   components: { Bar },
+  props: {
+    nativeCount: Number,
+    asianCount: Number,
+    blackCount: Number,
+    latinxCount: Number,
+    whiteCount: Number,
+    multiCount: Number,
+    unknownCount: Number
+  },
   data() {
     return {
       chartData: {
-        labels: [ 'January', 'February', 'March' ],
-  
-        datasets: [{ backgroundColor: ['#fb6f92', '#ff8fab', '#ffb3c6', '#ffc2d1', '#ffe5ec'],
-        data: [, 20, 12] } ]
+        labels: ['Native American', 'Asian', 'Black', 'Latinx', 'White', 'Multi Racial', 'Unknown'],
+        datasets: [{
+          backgroundColor: ['#fb6f92', '#ff8fab', '#ffb3c6', '#ffc2d1', '#ffe5ec'],
+          data: [this.nativeCount, this.asianCount, this.blackCount, this.latinxCount, this.whiteCount, this.multiCount, this.unknownCount]
+        }]
       },
       chartOptions: {
         responsive: true
       }
-    }
-  
+    };
   },
+};
+</script>
 
-  
 
-
-
+<style scoped>
+  h1{
+    text-align: center;
+  }
+  .container{
+  animation: loadIn 0.75s; 
+    display: grid;
+    grid-template-columns: 2fr 2fr;
+    padding: 0 2rem;
 }
 
-</script>
+@keyframes loadIn{
+  from{
+opacity: 0;
+transform: rotateX(-10deg);
+  }
+  to{
+opacity: 1;
+transform: rotateX(0);
+  }
+}
+
+</style>
