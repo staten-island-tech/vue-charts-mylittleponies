a22684eb717331b0c7513f22b4911821a3d07d66
Mandy <139280587+MandyyChen@users.noreply.github.com>
2024-04-02 00:50:09 -0400

    c

--- vue-project/src/components/PieGraph.vue
+++ vue-project/src/components/PieGraph.vue
@@ -19,25 +19,31 @@ ChartJS.register(ArcElement, Tooltip, Legend)
 export default {
   name: 'PieChart',
   components: { Pie },
+  props: {
+    nativeCount: Number,
+    asianCount: Number,
+    blackCount: Number,
+    latinxCount: Number,
+    whiteCount: Number,
+    multiCount: Number,
+    unknownCount: Number
+  },
   data() {
     return {
-        pieData: {
-        labels: ['Native American','Asian', 'White', 'Black', 'Latinx', 'Multi-Racial'],
-        datasets: [{ backgroundColor: ['#fb6f92', '#ff8fab', '#ffb3c6', '#ffc2d1', '#ffe5ec'],
-        data: [40,20,80,10]}]
-    },  
-    pieOptions: {
-    responsive: true,
-    maintainAspectRatio: true,
-  
-    }
-    }
+      pieData: {
+        labels: ['Native American', 'Asian', 'Black', 'Latinx', 'White', 'Multi Racial', 'Unknown'],
+        datasets: [{
+          label: ['Dataset'],
+          backgroundColor: ['#fb6f92', '#ff8fab', '#ffb3c6', '#ffc2d1', '#ffe5ec'],
+          data: [this.nativeCount, this.asianCount, this.blackCount, this.latinxCount, this.whiteCount, this.multiCount, this.unknownCount]
+        }]
+      },
+      pieOptions: {
+        responsive: true
+      }
+    };
   },
-
-}
-
-
-
+};
 </script>
 
 <style>
